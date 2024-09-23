69e5c0fe20a205ecf70f47f72270f48b49a93016
urodstvo <103985625+urodstvo@users.noreply.github.com>
2024-09-23 06:31:10 +0000

    feat(task1): added area to perfomance chart

--- Artificial Intelligence Systems/src/pages/task1/perfomance-chart.tsx
+++ Artificial Intelligence Systems/src/pages/task1/perfomance-chart.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import {
+  Area,
   CartesianGrid,
+  ComposedChart,
   Label,
   Line,
-  LineChart,
   XAxis,
   YAxis,
   type DefaultLegendContentProps,
 } from "recharts";
 
-import { ChartConfig, ChartContainer, ChartLegend } from "@/components/ui/chart";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+} from "@/components/ui/chart";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useContext, useMemo } from "react";
+import { QualityContext, SpeedContext } from ".";
+import {
+  highQualityFunc,
+  highSpeedFunc,
+  lowQualityFunc,
+  lowSpeedFunc,
+  medQualityFunc,
+  medSpeedFunc,
+} from "./functions";
 
 const CustomLegend = (props: DefaultLegendContentProps) => {
   const { payload } = props;
@@ -20,7 +35,10 @@ const CustomLegend = (props: DefaultLegendContentProps) => {
     <ul className="flex gap-2 justify-center mt-4">
       {payload?.map((entry, index) => (
         <li key={`item-${index}`} className="flex items-center gap-1">
-          <div className="w-2 h-2 rounded-full" style={{ backgroundColor: entry.color }} />
+          <div
+            className="w-2 h-2 rounded-full"
+            style={{ backgroundColor: entry.color }}
+          />
           {entry.value === "low" && "Низкая"}
           {entry.value === "med" && "Средняя"}
           {entry.value === "high" && "Высокая"}
@@ -34,14 +52,38 @@ const data = [
   { performance: 0, low: 1, med: undefined, high: undefined },
   { performance: 40, low: 1, med: 0, high: undefined },
   { performance: 50, low: 0, med: 1, high: undefined },
-  { performance: 70, low: undefined, med: 1, high: 0 },
-  { performance: 80, low: undefined, med: 0, high: 1 },
+  { performance: 65, low: undefined, med: 1, high: 0 },
+  { performance: 75, low: undefined, med: 0, high: 1 },
   { performance: 100, low: undefined, med: undefined, high: 1 },
 ];
 
 const chartConfig = {} satisfies ChartConfig;
 
 export const PerfomanceChart = () => {
+  const { speed } = useContext(SpeedContext);
+  const { quality } = useContext(QualityContext);
+
+  const isLow =
+    (lowSpeedFunc(speed) && lowQualityFunc(quality)) ||
+    (lowSpeedFunc(speed) && medQualityFunc(quality)) ||
+    (medSpeedFunc(speed) && lowQualityFunc(quality)) ||
+    (highSpeedFunc(speed) && lowQualityFunc(quality));
+
+  const isMed =
+    (lowSpeedFunc(speed) && highQualityFunc(quality)) ||
+    (medSpeedFunc(speed) && medQualityFunc(quality));
+
+  const isHigh =
+    (medSpeedFunc(speed) && highQualityFunc(quality)) ||
+    (highSpeedFunc(speed) && medSpeedFunc(quality)) ||
+    (highSpeedFunc(speed) && highQualityFunc(quality));
+
+  const dataWithRange = data.map((el) => ({
+    ...el,
+    rangeLow: isLow && el.low !== undefined ? [0, el.low] : undefined,
+    rangeMed: isMed && el.med !== undefined ? [0, el.med] : undefined,
+    rangeHigh: isHigh && el.high !== undefined ? [0, el.high] : undefined,
+  }));
   return (
     <Card className="shadow-none w-fit">
       <CardHeader className="items-center flex flex-col">
@@ -49,9 +91,9 @@ export const PerfomanceChart = () => {
       </CardHeader>
       <CardContent className="p-10">
         <ChartContainer config={chartConfig} className="w-[500px] h-[300px] ">
-          <LineChart
+          <ComposedChart
             accessibilityLayer
-            data={data}
+            data={dataWithRange}
             margin={{
               left: 12,
               right: 12,
@@ -76,12 +118,32 @@ export const PerfomanceChart = () => {
             </XAxis>
             <YAxis dataKey="" tickLine={false} tickMargin={8} max={1} />
 
-            <Line dataKey="low" type="linear" stroke="red" strokeWidth={2} dot={false} />
-            <Line dataKey="med" type="linear" stroke="green" strokeWidth={2} dot={false} />
-            <Line dataKey="high" type="linear" stroke="blue" strokeWidth={2} dot={false} />
-
+            <Line
+              dataKey="low"
+              type="linear"
+              stroke="red"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="med"
+              type="linear"
+              stroke="green"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="high"
+              type="linear"
+              stroke="blue"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Area dataKey="rangeLow" stroke="black" fill="black" />
+            <Area dataKey="rangeMed" stroke="black" fill="black" />
+            <Area dataKey="rangeHigh" stroke="black" fill="black" />
             <ChartLegend content={<CustomLegend />} />
-          </LineChart>
+          </ComposedChart>
         </ChartContainer>
       </CardContent>
     </Card>
